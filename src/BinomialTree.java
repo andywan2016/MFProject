8a8616fe1370f68ad2c1948993002a28bab6cd5f
andywan2016 <zw2327@columbia.edu>
2016-09-23 22:54:12 -0400

    good

---
+++ src/BinomialTree.java
@@ -0,0 +1,20 @@
+
+public class BinomialTree {
+	final int Steps=20;
+	double Pro_up;	
+    double[][] BiTree=new double[Steps][];
+    
+    
+    public BinomialTree(double Pro){
+    	Pro_up=Pro;
+    	for(int i=0;i<=Steps-1;i++)
+    	{
+    		BiTree[i]=new double[i+1];
+       	}
+    }
+    }
+    	
+    	
+    	
+    	
+    
