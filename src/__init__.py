8b9fda9c843a64ab04536bae65941d6705394e51
andywan2016 <zw2327@columbia.edu>
2016-11-15 20:10:37 -0500

    udpated

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,13 @@ import copy
 
 def getCurrPrice(tick,date):
 	stock=Share(tick)
+	try:
+		stock.get_historical(date,date)
+	except:
+		return None
+	
 	stock_detail=stock.get_historical(date,date)
+	#print stock_detail
 	if len(stock_detail) is 0:
 		return None
 	else:
@@ -15,18 +21,18 @@ def getCurrPrice(tick,date):
 def getPrice(tick,days): #(yahoo tick,latest date, how many days look back)
 	current_date=datetime.now()
 	#print current_date
-	day_left=days
-	day_left-=1
+	#day_left=days
+	#day_left-=1
 	#day_left=days
 	price_list=[]
 	loop_times=0 # use to control the times of try
 	#price=1
 	
-	while day_left>-1 and loop_times<20: #if try times greater than 100
+	while days>0 and loop_times<20: #if try times greater than 100
 		#print day_left
 		#price=0
 		cur_str= current_date.strftime("%Y-%m-%d")
-		#print cur_str
+		print cur_str
 		#pirce=getCurrPrice(tick,cur_str)
 		
 		#print getCurrPrice(tick,cur_str)
@@ -34,12 +40,13 @@ def getPrice(tick,days): #(yahoo tick,latest date, how many days look back)
 		if getCurrPrice(tick,cur_str) is None:
 			current_date=current_date-timedelta(days=1)
 			loop_times+=1
+			#days-=1
 			continue
 		else:
 			#print price
 			price_list.append(getCurrPrice(tick,cur_str))
 			current_date=current_date-timedelta(days=1)
-			day_left-=1
+			days-=1
 	return price_list
 	
 	
@@ -48,13 +55,18 @@ def getPrice(tick,days): #(yahoo tick,latest date, how many days look back)
 	#$print (current_date-timedelta(days=1)).strftime("%Y-%m-%d")
 	
 
-#a= float(getCurrPrice('YHOO','2016-11-11'))
+#print getCurrPrice('YHOO','2016-11-14')
 #print a
 #print type(a)
-#a= getPrice('YHOO',40)
+#a= getPrice('YHOO',30)
+#print a
 #print len(a)
 
-
-
-
-
+'''
+def getWildPrice(tick,date):
+	look_back_days=60
+	begin_date=datetime.now()-timedelta(days=look_back_days)
+	stock=Share(tick)
+	raw_list=
+'''
+	
