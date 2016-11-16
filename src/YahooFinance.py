812ca9a55250f4d19d9e42af2e8f6ff822be6e4a
andywan2016 <zw2327@columbia.edu>
2016-11-15 20:10:23 -0500

    updated

--- src/YahooFinance.py
+++ src/YahooFinance.py
@@ -3,30 +3,40 @@ from yahoo_finance import Share
 import sys
 import argparse
 import csv
-
+from datetime import datetime,timedelta,date
 
 #print len(sys.argv)
 #first agurment is yahoo finance instrument ticket
 
 #now tyring agurement paser
+#add share you wanna download here:
+ticketlist=["AAPL","YHOO"]
 
 if sys.argv[1]=="-f":  #this part is for loading mutiplule shares from list of csv file
-	file_path="../data/TicketList.csv"
+	#file_path="../data/TicketList.csv"
 	#with open(file_path) as csvfile:
 	#	ticket
 #	wih open(file_path) as ticketlist:
 #		for line in ticketlist:
 #			print line
-	ticketlist=open(file_path,'r')
+	#ticketlist=open(file_path,'r')
+
 	output_file=open("../data/Prices.csv","wb")
-	wr=csv.writer(output_file,dialect='excel')
-	for line in ticketlist:
-		print line
-		print type(line)
-		print getPrice(line,10)
-		
+	#wr=csv.writer(output_file,dialect='excel')
+	for ticket in ticketlist:
+		print ticket
+		#print type(ticket)
+		tmp_list=getPrice(ticket,30)
+		#ticket.extend(tmp_list)
+		print tmp_list
+		print ticket
+		output_file.write(ticket+',')
+		for e in tmp_list
+		#output_line=[ticket].extend(tmp_list)
+		#print output_line
 		#wr.writerows(ls)
-	ticketlist.close()
+
+	#ticketlist.close()
 	output_file.close()
 			
 
@@ -35,18 +45,17 @@ if sys.argv[1]=="-d": #for single historical data load
 	yahoo=Share(Tick)
 	print "The historical price is as followed:"
 	print yahoo.get_historical(str(sys.argv[3]),str(sys.argv[4]))
-
+'''
 if len(sys.argv)==2:
 	Tick=str(sys.argv[1])
 	yahoo=Share(Tick)
-	print "current price is :{}".format(yahoo.get_price())
+	#print "current price is :{}".format(yahoo.get_price())
 
-
-
-
+'''
+'''
 else:
 	print("the argument length is incorrect")
-
+'''
 
 #print yahoo.get_historical('2016-04-25','2016-05-05')
 
